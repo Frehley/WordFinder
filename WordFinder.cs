46881b198c28d6b1d798178e59fbcae9c89e7627
dzuccoli <dzuccoli@cfotechlatam.com>
2021-07-22 19:12:25 -0300

    Small correction

--- WordFinder.cs
+++ WordFinder.cs
@@ -25,8 +25,7 @@ namespace WF
             // I check how many words Im searching. Just to be sure...
             Console.WriteLine(string.Concat("There are: ", wordstream.Count(), " words in the list"));
 
-            //Since X and Y are equals I use .Count in both directions
-            StringBuilder sVertical = new StringBuilder("", _matrix.Count());
+
 
             List<string> fWords = new List<string>();
             List<string> verticalWords = new List<string>();
@@ -35,12 +34,15 @@ namespace WF
             //I rearrange every char in the vertical dimension to be able to pass it to a string.
             for (int col = 0; col < _matrix.Count(); col++)
             {
+                //Since X and Y are equals I use .Count in both directions
+                StringBuilder sVertical = new StringBuilder("", _matrix.Count());
+
                 foreach (string item in _matrix)
                 {
                     sVertical.Append(item.Substring(col, 1));
                 }
 
-                //Console.WriteLine(sVertical.ToString());
+                Console.WriteLine(sVertical.ToString());
                 verticalWords.Add(sVertical.ToString());
             }
 
